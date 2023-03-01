dd162c04a0af8d28fd8030c0f09855c873e19b82
MURABIT-PASHA <akdoganmurabit@gmail.com>
2023-03-01 16:20:37 +0300

    Pivot #1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
 
+
 # Define the main app class that inherits from MDApp
 class MainApp(MDApp):
     floating_action_options = {
@@ -11,18 +12,18 @@ class MainApp(MDApp):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.label = None  # Initialize label attribute to None
         self.counter = 0  # Initialize counter attribute to zero
 
     # Define the build method that returns the app's UI
     def build(self):
         self.theme_cls.theme_style = "Dark"  # or "Light", "normal", "darkest"
+        # self.theme_cls.primary_color = (0.05, 0.07, 0.09, 1)
         return Builder.load_file('home.kv')
 
     # Define a method to increment the counter and update the label text
     def increment_counter(self):
         self.counter += 1
-        self.label.text = str(self.counter)
+        self.root.ids.label.text = str(self.counter)
 
 
 # Create an instance of the MainApp class and run it
