a6736587eb44d6afff3c48f5fdf83caf81c46337
MURABIT-PASHA <akdoganmurabit@gmail.com>
2023-03-14 18:24:07 +0300

    Splash Screen Bugs Fixed

--- main.py
+++ main.py
@@ -1,39 +1,48 @@
-# Import necessary Kivy and KivyMD modules
+from kivy.animation import Animation
 from kivy.clock import Clock
 from kivy.core.window import Window
-from kivy.uix.screenmanager import ScreenManager
 from kivy.lang import Builder
+from kivy.properties import ListProperty
+from kivymd.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
 
 Window.size = (350, 580)
 
 
-# Define the main app class that inherits from MDApp
 class MainApp(MDApp):
     floating_action_options = {
         'Camera': 'camera',
         'Gallery': 'image'
     }
+    items = ListProperty([])
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.address = None
+        self.founded_devices = None
+        self.home_screen = Builder.load_file('home.kv')
+        self.splash_screen = Builder.load_file('splash.kv')
+        self.screen_manager = ScreenManager()
 
     # Define the build method that returns the app's UI
     def build(self):
-        global screen_manager
-        screen_manager = ScreenManager()
-        screen_manager.add_widget(Builder.load_file("splash.kv"))
-        screen_manager.add_widget(Builder.load_file("login.kv"))
-        screen_manager.add_widget(Builder.load_file("home.kv"))
+        self.screen_manager.add_widget(self.splash_screen)
+        self.screen_manager.add_widget(self.home_screen)
         self.theme_cls.theme_style = "Dark"
-        # self.theme_cls.primary_color = (0.05, 0.07, 0.09, 1)
-        return screen_manager
+        return self.screen_manager
 
     def on_start(self):
-        Clock.schedule_once(self.login, 5)
+        Clock.schedule_once(self.go_to_home, 3)
 
-    def login(self, *args):
-        screen_manager.current = "login"
+    def splash_animation(self, **kwargs):
+        if kwargs['logo'] is not None and kwargs['text'] is not None:
+            logo_anim = Animation(pos_hint={"center_x": .2, "center_y": .55}, duration=1)
+            logo_anim.start(kwargs['logo'])
+            text_anim = Animation(opacity=1, duration=3)
+            text_anim.start(kwargs['text'])
+    def go_to_home(self, *args):
+        self.screen_manager.current = self.home_screen.name
+
 
 if __name__ == "__main__":
     MainApp().run()
